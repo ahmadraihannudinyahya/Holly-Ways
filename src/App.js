5c6f760f14f8848e705430446118c118d22894cb
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-16 07:53:50 +0700

    add profil page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Main from "./pages/Main";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Detail from "./pages/Detail";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/profile/:id" component={Profile} />
         <Route path="/fund/:id" component={Detail} />
         <Route path="/" component={Main} />
       </Switch>
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import Navbar from "../component/common/Navbar";
+
+function Profile(){
+  return(
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Profile;
