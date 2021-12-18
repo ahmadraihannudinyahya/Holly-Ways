002f8ce8bc79364722361ca13b713e3c566be3e8
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-18 22:25:55 +0700

    implemented component dropdown

--- src/component/common/Navbar.js
+++ src/component/common/Navbar.js
@@ -1,20 +1,28 @@
+import { useState } from 'react';
 import { useHistory } from 'react-router';
 import DropDown from './DropDown';
 import './Navbar.css'
 
 function Navbar(props){
   const {handleTogleModalLogin, handleTogleModalRegister, isLogin} = props;
+
+  const [state, setState] = useState({
+    dropdownStatus:false,
+  })
   const history = useHistory();
   const handleIconClick = (e)=>{
     e.preventDefault();
     history.push('/')
   }
+  const togleDropdown=()=>{
+    setState(prevState => ({dropdownStatus : !prevState.dropdownStatus}))
+  }
   return (
   <div className="navbar">
       <img src="/Icon.png" alt="icon" onClick = {handleIconClick}/>
       <div>
-        {isLogin? <img src="/Icon.png" alt="avatar" />: <><button id="login" onClick={handleTogleModalLogin}>Login</button><button id="register" onClick={handleTogleModalRegister}>Register</button> </>}
-        <DropDown />
+        {isLogin? <img src="/Icon.png" alt="avatar" onClick={togleDropdown}/>: <><button id="login" onClick={handleTogleModalLogin}>Login</button><button id="register" onClick={handleTogleModalRegister}>Register</button> </>}
+        {state.dropdownStatus?<DropDown />:<></>}
       </div>
     </div>
   )
